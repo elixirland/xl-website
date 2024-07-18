2de79c4fa9dbefd576ea45540099e444248b198d
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-07-18 14:31:45 +0200

    Add page controller tests

--- test/xl_website_web/controllers/page_controller_test.exs
+++ test/xl_website_web/controllers/page_controller_test.exs
@@ -1,8 +1,34 @@
 defmodule XlWebsiteWeb.PageControllerTest do
   use XlWebsiteWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Compact Elixir challenges with idomatic code examples"
+  describe "GET /" do
+    test "renders home page", %{conn: conn} do
+      conn = get(conn, "/")
+      assert html_response(conn, 200) =~ "Compact Elixir challenges with idomatic code examples"
+    end
+  end
+
+  describe "GET /challenges" do
+    test "renders challenges page", %{conn: conn} do
+      conn = get(conn, "/challenges")
+      assert html_response(conn, 200) =~ "Challenges"
+    end
+
+    test "renders Book Club challenge", %{conn: conn} do
+      conn = get(conn, "/challenges")
+      assert html_response(conn, 200) =~ "Book Club"
+    end
+  end
+
+  describe "GET /about" do
+    test "renders about page", %{conn: conn} do
+      conn = get(conn, "/about")
+      assert html_response(conn, 200) =~ "About"
+    end
+  end
+
+  test "Invalid path" do
+    conn = get(build_conn(), "/invalid")
+    assert html_response(conn, 404) =~ "Not Found"
   end
 end
