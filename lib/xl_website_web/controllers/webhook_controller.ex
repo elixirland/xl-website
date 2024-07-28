b5660ff7066e0e05b55d0971e01d883e055b3f3e
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-07-28 21:59:37 +0200

    Add additional webhook controller tests

--- lib/xl_website_web/controllers/webhook_controller.ex
+++ lib/xl_website_web/controllers/webhook_controller.ex
@@ -3,23 +3,16 @@ defmodule XlWebsiteWeb.WebhookController do
   require Logger
   alias XlWebsiteWeb.HTTPClient
   alias XlWebsite.Parser
+  alias XlWebsite.Exercises
 
   def push(conn, _params) do
     with {:ok, signature} <- get_signature(conn),
          {:ok, secret} <- get_hashed_secret(signature),
          {:ok, _} <- check_valid_secret(conn.private[:raw_body], secret) do
-      full_name = conn.body_params["repository"]["full_name"]
-      raw_topics = conn.body_params["repository"]["topics"]
-
-      XlWebsite.Exercises.upsert_exercise(%{
-        full_name: full_name,
-        name: Parser.build_name(full_name),
-        slug: Parser.build_slug(full_name),
-        html_url: conn.body_params["repository"]["html_url"],
-        description: conn.body_params["repository"]["description"],
-        topics: Parser.parse_topics(raw_topics),
-        readme_md: fetch_readme(full_name)
-      })
+      conn.body_params
+      |> extract_exercise_attrs()
+      |> Exercises.upsert_exercise()
+      |> IO.inspect()
 
       resp(conn, 200, "ok")
     else
@@ -44,6 +37,22 @@ defmodule XlWebsiteWeb.WebhookController do
         |> put_status(:bad_request)
         |> json(%{message: "invalid secret"})
     end
+  end
+
+  @spec extract_exercise_attrs(body_params :: map()) :: map()
+  defp extract_exercise_attrs(body_params) do
+    full_name = body_params["repository"]["full_name"]
+    raw_topics = body_params["repository"]["topics"]
+
+    %{
+      full_name: full_name,
+      name: Parser.build_name(full_name),
+      slug: Parser.build_slug(full_name),
+      html_url: body_params["repository"]["html_url"],
+      description: body_params["repository"]["description"],
+      topics: Parser.parse_topics(raw_topics),
+      readme_md: fetch_readme(full_name)
+    }
   end
 
   defp get_signature(conn) do
--- test/xl_website_web/controllers/webhook_controller_test.exs
+++ test/xl_website_web/controllers/webhook_controller_test.exs
@@ -1,15 +1,68 @@
 defmodule XlWebsiteWeb.WebhookControllerTest do
   use XlWebsiteWeb.ConnCase
+  import XlWebsite.Factory
   import Plug.Conn
+  import Ecto.Query, only: [from: 2]
 
   describe "POST /webhooks/push" do
     setup %{conn: conn} do
       secret = Application.get_env(:xl_website, :github_webhooks_secret)
 
       body =
-        "{\"repository\":{\"full_name\":\"elixirland/xle-username-generator\",\"topics\":[\"topic A\",\"topic B\"]}}"
+        "{\"repository\":{\"description\":\"Some description.\",\"full_name\":\"elixirland/xle-username-generator\",\"html_url\":\"https://example.com/webhooks\",\"topics\":[\"topic A\",\"topic B\"]}}"
 
       %{conn: conn, body: body, secret: secret}
+    end
+
+    test "creates a new exercise",
+         %{conn: conn, body: body, secret: secret} do
+      signature =
+        :crypto.mac(:hmac, :sha256, secret, body)
+        |> Base.encode16(case: :lower)
+        |> then(&"sha256=#{&1}")
+
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> put_req_header("x-hub-signature-256", signature)
+        |> post("/webhooks/push", body)
+
+      refute nil ==
+               XlWebsite.Repo.one(
+                 from(e in XlWebsite.Exercises.Exercise,
+                   where: e.full_name == "elixirland/xle-username-generator"
+                 )
+               )
+
+      assert response(conn, :ok) =~ "ok"
+    end
+
+    test "updates an existing exercise",
+         %{conn: conn, body: body, secret: secret} do
+      insert!(:exercise,
+        full_name: "elixirland/xle-username-generator",
+        topics: ["Topic X", "Topic Y"]
+      )
+
+      signature =
+        :crypto.mac(:hmac, :sha256, secret, body)
+        |> Base.encode16(case: :lower)
+        |> then(&"sha256=#{&1}")
+
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> put_req_header("x-hub-signature-256", signature)
+        |> post("/webhooks/push", body)
+
+      assert %{topics: ["Topic a", "Topic b"]} =
+               XlWebsite.Repo.one(
+                 from(e in XlWebsite.Exercises.Exercise,
+                   where: e.full_name == "elixirland/xle-username-generator"
+                 )
+               )
+
+      assert response(conn, :ok) =~ "ok"
     end
 
     test "returns ok when secret is valid",
