e8a75243ac51cd3bbba6a720b64d845ba7c3494e
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-08-21 13:42:35 +0200

    Tweak ecosystem page content

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,8 +23,6 @@ import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import ColorScheme from "./color_scheme"
 
-console.log(ColorScheme)
-
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
