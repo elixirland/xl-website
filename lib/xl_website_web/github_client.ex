931320ded7a0a7ecd8893657b46da852a6ba79c4
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-08-25 09:54:25 +0200

    Format

--- lib/xl_website_web/github_client.ex
+++ lib/xl_website_web/github_client.ex
@@ -4,7 +4,10 @@ defmodule XlWebsiteWeb.GithubClient do
 
   def fetch_ecosystem_md(full_name) do
     http_request =
-      HTTPClient.build(:get, "https://raw.githubusercontent.com/#{full_name}/main/ECOSYSTEM.md")
+      HTTPClient.build(
+        :get,
+        "https://raw.githubusercontent.com/#{full_name}/main/ECOSYSTEM.md"
+      )
 
     case HTTPClient.request(http_request, XlWebsite.Finch) do
       {:ok, %Finch.Response{status: 200, body: body}} ->
@@ -20,7 +23,10 @@ defmodule XlWebsiteWeb.GithubClient do
 
   def fetch_readme_md(full_name) do
     http_request =
-      HTTPClient.build(:get, "https://raw.githubusercontent.com/#{full_name}/main/README.md")
+      HTTPClient.build(
+        :get,
+        "https://raw.githubusercontent.com/#{full_name}/main/README.md"
+      )
 
     case HTTPClient.request(http_request, XlWebsite.Finch) do
       {:ok, %Finch.Response{status: 200, body: body}} ->
