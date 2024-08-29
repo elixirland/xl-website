18624b78a22bc36ff743a2e63020843bb4b1a258
Coen Bakker <106626890+coenbakker@users.noreply.github.com>
2024-08-29 10:17:20 +0200

    Add play on hover for preview videos on project cards

--- assets/js/card_media.js
+++ assets/js/card_media.js
@@ -6,32 +6,164 @@ const CardMedia = {
     
     if (cardMediaElements.length === 0) return
 
-    cardMediaElements.forEach((cardMediaElement) => {
-      const img = cardMediaElement.querySelector("img")
-      const video = cardMediaElement.querySelector("video")
+    const mediaQuery = window.matchMedia("(max-width: 500px)")
 
-      img.hasBrokenSrc = false
+    if (mediaQuery.matches) {
+      // On scroll check if a thumbnail of thumbnail is in the center of the viewport
+      // If it is, play the video
+      let debounceTimeout
 
-      img.onerror = () => {
-        img.style.display = "none"
-        img.hasBrokenSrc = true
-        video.style.display = "block"
-        video.play()
-      }
+      window.addEventListener("scroll", () => {
 
-      img.onmouseenter = () => {
-        img.style.display = "none"
-        video.style.display = "block"
-        video.play()
-      }
+        clearTimeout(debounceTimeout)
 
-      video.onmouseleave = () => {
-        if (!img.hasBrokenSrc) {
-          img.style.display = "block"
-          video.style.display = "none"
+        cardMediaElements.forEach((cardMediaElement) => {
+          const img = cardMediaElement.querySelector("img")
+          const video = cardMediaElement.querySelector("video")
+
+          if (img.hasBrokenSrc) return
+
+          const rect = cardMediaElement.getBoundingClientRect()
+          const windowHeight = window.innerHeight || document.documentElement.clientHeight
+          const windowWidth = window.innerWidth || document.documentElement.clientWidth
+
+          const verticalMargin = (windowHeight - rect.height) / 2 * 0.66
+
+          const isElementInViewport = (
+            rect.top >= verticalMargin &&
+            rect.left >= 0 &&
+            rect.bottom <= windowHeight - verticalMargin &&
+            rect.right <= windowWidth
+          )
+
+          if (isElementInViewport) {
+            debounceTimeout = setTimeout(() => {
+              img.style.display = "none"
+              video.style.display = "block"
+              video.play()
+            }, 2000)
+          } else {
+            img.style.display = "block"
+            video.style.display = "none"
+            video.pause()
+            video.currentTime = 0
+          }
+
+          video.onended = () => {
+            img.style.display = "block"
+            video.style.display = "none"
+          }
+        })
+      })
+    } else {
+      // On hover, play the video
+      cardMediaElements.forEach((cardMediaElement) => {
+        const img = cardMediaElement.querySelector("img")
+        const video = cardMediaElement.querySelector("video")
+  
+        img.hasBrokenSrc = false
+  
+        img.onerror = () => {
+          img.style.display = "none"
+          img.hasBrokenSrc = true
+          video.style.display = "block"
+          video.play()
         }
+  
+        img.onmouseenter = () => {
+          img.style.display = "none"
+          video.style.display = "block"
+          video.play()
+        }
+  
+        video.onmouseleave = () => {
+          if (!img.hasBrokenSrc) {
+            img.style.display = "block"
+            video.style.display = "none"
+          }
+          video.pause()
+          video.currentTime = 0
+        }
+      })
+    }
+
+    // TODO: Instead of viewport size, let user agent type decide what triggers
+    // video playback
+
+    window.addEventListener("resize", () => {
+
+      cardMediaElements.forEach((cardMediaElement) => {
+        const img = cardMediaElement.querySelector("img")
+        const video = cardMediaElement.querySelector("video")
+
+        img.style.display = "block"
+        video.style.display = "none"
         video.pause()
         video.currentTime = 0
+      })
+
+      const windowWidth = window.innerWidth || document.documentElement.clientWidth
+
+      if (windowWidth < 500) {
+        window.addEventListener("scroll", () => {
+          clearTimeout(debounceTimeout)
+
+          cardMediaElements.forEach((cardMediaElement) => {
+            const img = cardMediaElement.querySelector("img")
+            const video = cardMediaElement.querySelector("video")
+
+            if (img.hasBrokenSrc) return
+
+            const rect = cardMediaElement.getBoundingClientRect()
+            const windowHeight = window.innerHeight || document.documentElement.clientHeight
+            const windowWidth = window.innerWidth || document.documentElement.clientWidth
+
+            const verticalMargin = (windowHeight - rect.height) / 2 * 0.66
+
+            const isElementInViewport = (
+              rect.top >= verticalMargin &&
+              rect.left >= 0 &&
+              rect.bottom <= windowHeight - verticalMargin &&
+              rect.right <= windowWidth
+            )
+
+            if (isElementInViewport) {
+              debounceTimeout = setTimeout(() => {
+                img.style.display = "none"
+                video.style.display = "block"
+                video.play()
+              }, 2000)
+            } else {
+              img.style.display = "block"
+              video.style.display = "none"
+              video.pause()
+              video.currentTime = 0
+            }
+
+            video.onended = () => {
+              img.style.display = "block"
+              video.style.display = "none"
+            }
+          })
+        })
+      } else {
+        cardMediaElements.forEach((cardMediaElement) => {
+          const img = cardMediaElement.querySelector("img")
+          const video = cardMediaElement.querySelector("video")
+
+          img.onmouseenter = () => {
+            img.style.display = "none"
+            video.style.display = "block"
+            video.play()
+          }
+
+          video.onmouseleave = () => {
+            img.style.display = "block"
+            video.style.display = "none"
+            video.pause()
+            video.currentTime = 0
+          }
+        })
       }
     })
   }
